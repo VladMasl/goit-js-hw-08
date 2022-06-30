18a18ae7a51499947231f5b6d1eb1e16043d72b3
Vlad  Masliuk <102538039+VladMasl@users.noreply.github.com>
2022-06-30 17:48:36 +0300

    1 - 2 taskFinish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframeEl = document.querySelector('iframe');
+
+const player = new Player(iframeEl);
+
+VIDEO_TIME_PROGRES_KEY = 'videoplayer-current-time';
+
+const currentVideoTime = e => {
+  const elSrtingifi = JSON.stringify(e);
+  localStorage.setItem(VIDEO_TIME_PROGRES_KEY, elSrtingifi);
+};
+
+const savedVideoTime = () => {
+  const savedTime = localStorage.getItem(VIDEO_TIME_PROGRES_KEY);
+  const parsedTime = JSON.parse(savedTime);
+
+  if (!parsedTime) {
+    return {};
+  }
+
+  if (savedTime) {
+    player.setCurrentTime(parsedTime.seconds);
+  }
+};
+
+savedVideoTime();
+
+player.on('timeupdate', throttle(currentVideoTime, 1000));
